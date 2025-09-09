856d0b058f8962a04a93537b14376e693e080575
Kamran Alam <Kamrankhan0368@gmail.com>
2025-09-09 23:46:05 +0500

    Updated

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-var inputElemnt = document.getElementById("inputText")
+// var inputElemnt = document.getElementById("inputText")
 
 
 
 
-function add()
-{
-    localStorage.setItem("inputValues",inputElemnt.value);
-    inputElemnt.value = ""
-}
+// function add()
+// {
+//     localStorage.setItem("inputValues",inputElemnt.value);
+//     inputElemnt.value = ""
+//     getitm()
+// }
+
+// function getitm()
+// {
+//     console.log(localStorage.getItem("inputValues"))
+// }
+
+var apn = document.querySelector("h2")
+apn.innerText += " apna college";
+
+
+
+console.log(apn);
