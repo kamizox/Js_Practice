50dfa885edc2ea9b3952c0a943221a9e14c6e049
Kamran Alam <Kamrankhan0368@gmail.com>
2025-09-10 01:37:09 +0500

    Update

--- index.js
+++ index.js
@@ -15,9 +15,59 @@
 //     console.log(localStorage.getItem("inputValues"))
 // }
 
-var apn = document.querySelector("h2")
-apn.innerText += " apna college";
 
+// DOM Practice Questions (Next Level)
 
+// Qs.1 Create a H2 heading element with text - "Hello JavaScript". Append "from Apna College students" to this text using JS.
 
-console.log(apn);
+// Qs.2 Create 3 divs with common class name "box". Access them & add some unique text to each of them.
+
+// Q3. Create a button with text “Click Me”.
+
+// 👉 Jab button pe click ho to ek alert aaye: “Button was clicked!”
+
+// Q4. Create a paragraph <p> with text “Original Text”.
+
+// 👉 Ek button banao jo click hote hi text ko change kar de to “Text has been changed!”
+
+// Q5. Create an input box aur ek button.
+
+// 👉 Jab user input me apna naam likhe aur button dabaye → neeche <p> me show ho “Hello [naam]”
+
+// Q6. Create 5 list items <li> inside a <ul>.
+
+// 👉 JavaScript se un sab ke text ko change karo → "Item 1", "Item 2", … "Item 5".
+
+// Q7. Create a <div> with some text.
+
+// 👉 JavaScript se us div ka background color aur font color change kar do.
+
+// Q8. Create 3 buttons: Red, Green, Blue.
+
+// 👉 Jab user kisi button pe click kare → <body> ka background us color me change ho jaye.
+
+// ⚡ Tip: Ye questions karte hue tum querySelector, querySelectorAll, textContent, innerText, style, addEventListener sab practice kar loge.
+// var apn = document.querySelector("h2")
+// apn.innerText += " apna college";
+
+// Question 2 Asnwer
+// var divs = document.querySelectorAll(".am")
+// divs[0].innerText += " Hy";
+// divs[1].innerText += "Hello";
+// divs[2].innerText += "Hi";
+
+// Question 3 Asnwer
+// function clickme()
+// {
+//     alert("button was clicked")
+// }
+
+// Question 4 Asnwer
+// function textchnge()
+// {
+//     var txtc = document.querySelector(".txt")
+//     txtc.innerText = "hm";
+// }
+
+// Question 5 Asnwer
+
